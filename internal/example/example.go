4c99359bc97367b3b7ad876edf61c7779d1ea57b
taako-502 <i.am.armorknight@gmail.com>
2023-12-26 23:05:04 +0200

    refactor: サンプルプログラムをexample.goに移動

---
+++ internal/example/example.go
@@ -0,0 +1,45 @@
+package example
+
+import (
+	"context"
+	"fmt"
+	customer_infrastructure "go-mongodb-sample/infrastructures/customers"
+	product_infrastructure "go-mongodb-sample/infrastructures/products"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Exammple(connectionString string, ctx context.Context, client *mongo.Client) {
+
+	// カスタマーを作成
+	c := customer_infrastructure.NewCustomer(ctx,
+		client.Database("testdb").Collection("customer"),
+	)
+	dto := customer_infrastructure.NewCustomerDTO("Alice", "alice@gmail.com", "Tokyo", nil)
+	customer, err := c.Create(dto)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", customer)
+
+	// プロダクトを作成
+	p := product_infrastructure.NewProduct(ctx,
+		client.Database("testdb").Collection("product"),
+	)
+	productDto := product_infrastructure.NewProductDTO("Apple", "iPhone", 100000, 10, "Smartphone")
+	product, err := p.Create(productDto)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", product)
+
+	// ドキュメントを取得するクエリ
+	findedCustomer, err := c.FindOne(customer.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Found document: ", findedCustomer)
+}
--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
-	customer_infrastructure "go-mongodb-sample/infrastructures/customers"
-	product_infrastructure "go-mongodb-sample/infrastructures/products"
+	"go-mongodb-sample/internal/example"
 	"log"
 	"time"
 
@@ -12,10 +10,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	// MongoDBサーバーへの接続文字列
-	connectionString := "mongodb://localhost:27017"
+// MongoDBサーバーへの接続文字列
+const connectionString = "mongodb://localhost:27017"
 
+func main() {
 	// コンテキストを設定
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -25,34 +23,6 @@ func main() {
 	}
 	defer client.Disconnect(ctx)
 
-	// カスタマーを作成
-	c := customer_infrastructure.NewCustomer(ctx,
-		client.Database("testdb").Collection("customer"),
-	)
-	dto := customer_infrastructure.NewCustomerDTO("Alice", "alice@gmail.com", "Tokyo", nil)
-	customer, err := c.Create(dto)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Inserted a single document: ", customer)
-
-	// プロダクトを作成
-	p := product_infrastructure.NewProduct(ctx,
-		client.Database("testdb").Collection("product"),
-	)
-	productDto := product_infrastructure.NewProductDTO("Apple", "iPhone", 100000, 10, "Smartphone")
-	product, err := p.Create(productDto)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Inserted a single document: ", product)
-
-	// ドキュメントを取得するクエリ
-	findedCustomer, err := c.FindOne(customer.ID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Found document: ", findedCustomer)
+	// サンプルを実行
+	example.Exammple(connectionString, ctx, client)
 }
