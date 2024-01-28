b0f94ab6a8f747a215bfb76591e54e435a2556c5
taako-502 <i.am.armorknight@gmail.com>
2024-01-28 21:31:15 +0200

    chore: プロモーション有効期限用のインデックスを追加

---
+++ migrations/3_add-promotioin-product-index.go
@@ -0,0 +1,28 @@
+package migrations
+
+import (
+	"context"
+	"os"
+
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func init() {
+	migrate.Register(func(db *mongo.Database) error {
+		if _, err := db.Collection(os.Getenv("PRODUCT_COLLECTION_NAME")).Indexes().CreateOne(context.TODO(), mongo.IndexModel{
+			Keys:    bson.D{{Key: "promotion_expires_at", Value: 1}},
+			Options: options.Index().SetExpireAfterSeconds(0),
+		}); err != nil {
+			return err
+		}
+		return nil
+	}, func(db *mongo.Database) error {
+		if _, err := db.Collection(os.Getenv("PRODUCT_COLLECTION_NAME")).Indexes().DropOne(context.TODO(), "promotion_expires_at_1"); err != nil {
+			return err
+		}
+		return nil
+	})
+}
