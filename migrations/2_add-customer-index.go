3b55b71791bce38556612e70b46bc750a4602412
taako-502 <i.am.armorknight@gmail.com>
2024-01-28 19:41:51 +0200

    chore: マイグレーションを追加（make downするとコレクションが消える）

---
+++ migrations/2_add-customer-index.go
@@ -0,0 +1,29 @@
+package migrations
+
+import (
+	"context"
+	"os"
+
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func init() {
+	migrate.Register(func(db *mongo.Database) error {
+		customerIndexModel := mongo.IndexModel{
+			Keys:    bson.D{{Key: "email", Value: 1}}, // 1は昇順を意味する
+			Options: options.Index().SetUnique(true),
+		}
+		if _, err := db.Collection(os.Getenv("CUSTOMER_COLLECTION_NAME")).Indexes().CreateOne(context.TODO(), customerIndexModel); err != nil {
+			return err
+		}
+		return nil
+	}, func(db *mongo.Database) error {
+		if _, err := db.Collection(os.Getenv("CUSTOMER_COLLECTION_NAME")).Indexes().DropOne(context.TODO(), "email_1"); err != nil {
+			return err
+		}
+		return nil
+	})
+}
